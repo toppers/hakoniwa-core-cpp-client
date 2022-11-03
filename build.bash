3669342a09aa2e883f04fd0750148ab09c3eda21
Takashi Mori <kanetugu2015@gmail.com>
2022-11-03 15:27:27 +0900

    updated for latest

--- build.bash
+++ build.bash
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+OS_TYPE="posix"
+uname -a | grep Linux > /dev/null
+if [ $? -ne 0 ]
+then
+    OS_TYPE="win"
+fi
+
 if [ $# -eq 0 ]
 then
 	cd cmake-build
-	cmake ..
+    if [ ${OS_TYPE} = "posix" ]
+    then
+        cmake ..
+    else
+        cmake .. -G "MSYS Makefiles"
+    fi
 	make
 else
 	rm -rf ./cmake-build/*
