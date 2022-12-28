d1e0b30f1df710afd091872421a3c695776542d6
Takashi Mori <kanetugu2015@gmail.com>
2022-12-28 09:54:01 +0900

    revert to shared memory and semaphore

---
+++ install.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -d /usr/local/bin/hakoniwa ]
+then
+	:
+else
+	sudo mkdir /usr/local/bin/hakoniwa
+fi
+
+sudo cp cmake-build/core/sample/base-procs/hako-cmd/hako-cmd /usr/local/bin/hakoniwa/
+sudo cp cmake-build/src/hakoc/libshakoc.* /usr/local/lib/hakoniwa/
