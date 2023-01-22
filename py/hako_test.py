ed838adea77549ef7edf16ac6fee62c2f77e214d
Takashi Mori <kanetugu2015@gmail.com>
2023-01-22 15:12:38 +0900

    add signal handler

--- py/hako_test.py
+++ py/hako_test.py
@@ -7,6 +7,14 @@ from binary import binary_writer
 from binary import binary_reader
 import hako_env
 import time
+import signal
+
+def handler(signum, frame):
+  print(f'SIGNAL(signum={signum})')
+  sys.exit(0)
+
+# signal.SIGALRMのシグナルハンドラを登録
+signal.signal(signal.SIGINT, handler)
 
 #TODO get ai model
 pass
@@ -18,9 +26,15 @@ env = hako_env.make("base_practice_1", "ev3")
 total_time = 0
 done = False
 env.reset()
-print("reset done")
+
 robo = env.robo()
-while not done and total_time < 10000:
+#10secs
+while not done and total_time < 1000:
     robo.foward(10)
     state, reward, done, info = env.step()
+    total_time = total_time + 1
+
+print("END")
+env.reset()
+sys.exit(0)
 
