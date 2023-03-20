db67cc6eb695719f6fe26fa2614b1b29dc70bf69
森崇 <kanetugu2015@gmail.com>
2023-03-21 06:52:01 +0900

    add summary of test results

--- test.bash
+++ test.bash
@@ -6,6 +6,9 @@ export PYTHONPATH="/usr/local/lib/hakoniwa/py:$PYTHONPATH"
 
 cd test
 
+TEST_COUNT=0
+TEST_PASSED=0
+TEST_FAILED=0
 for item in `ls`
 do
     if [ -d $item ]
@@ -13,7 +16,29 @@ do
         bash cleanup.bash
         cd $item
         python3 -m unittest
+        TEST_RESULT=$?
         cd ..
         bash cleanup.bash
+        if [ $TEST_RESULT -ne 0 ]
+        then
+            echo "TEST FAILED: $item"
+            TEST_FAILED=`expr ${TEST_FAILED} + 1`
+        else
+            echo "TEST PASSED: $item"
+            TEST_PASSED=`expr ${TEST_PASSED} + 1`
+        fi
+        TEST_COUNT=`expr ${TEST_COUNT} + 1`
     fi
 done
+
+echo "TEST SUMMARY:"
+echo "TOTAL : ${TEST_COUNT}"
+echo "PASSED: ${TEST_PASSED}"
+echo "FALIED: ${TEST_FAILED}"
+
+if [ $TEST_FAILED -gt 0 ]
+then
+    exit 1
+else
+    exit 0
+fi
