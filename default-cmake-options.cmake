808824081a59891d6db6959844a64f6d71f64e8d
Takashi Mori <kanetugu2015@gmail.com>
2024-01-14 08:41:37 +0900

    add fpic option

--- default-cmake-options.cmake
+++ default-cmake-options.cmake
@@ -31,6 +31,7 @@ else()
     set(CMAKE_USE_WIN32_THREADS_INIT 0)
     set(CMAKE_USE_PTHREADS_INIT 1)
     set(THREADS_PREFER_PTHREAD_FLAG ON)
+    add_definitions(-fPIC)
 endif()
 
 set(GCOV "disabled")
