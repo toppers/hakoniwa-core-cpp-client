582706d1b5bde871d3148815253c0e6eb83f94ff
Takashi Mori <kanetugu2015@gmail.com>
2024-02-07 17:23:28 +0900

    bug fixed

--- bindings/python/install.bash
+++ bindings/python/install.bash
@@ -17,4 +17,10 @@ sudo cp -rp bindings/python/hako_binary/offset /usr/local/lib/hakoniwa/hako_bina
 sudo cp -rp bindings/python/hako_binary/*.py /usr/local/lib/hakoniwa/py/hako_binary/
 sudo cp  bindings/python/hako_pdu.py /usr/local/lib/hakoniwa/py/
 
-sudo cp cmake-build/bindings/python/libhako_asset_python.* /usr/local/lib/hakoniwa/py/
+OS_TYPE=`uname`
+if [ $OS_TYPE = "Darwin" ]
+then
+	sudo cp cmake-build/bindings/python/libhako_asset_python.dylib /usr/local/lib/hakoniwa/py/hako_asset.so
+else
+	sudo cp cmake-build/bindings/python/libhako_asset_python.so /usr/local/lib/hakoniwa/py/hako_asset.so
+fi
