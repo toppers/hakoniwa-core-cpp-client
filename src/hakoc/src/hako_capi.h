1ca208c6ea568f3dc24cad1d468310cc88d46609
Takashi Mori <kanetugu2015@gmail.com>
2024-02-23 07:49:00 +0900

    updated for win dll

--- src/hakoc/src/hako_capi.h
+++ src/hakoc/src/hako_capi.h
@@ -8,54 +8,64 @@ extern "C" {
 
 #include "hako_capi_types.h"
 
+#define HAKO_EXPORTS
+#ifdef _WIN32
+#ifdef HAKO_EXPORTS
+#define HAKO_API __declspec(dllexport)
+#else
+#define HAKO_API __declspec(dllimport)
+#endif
+#else
+#define HAKO_API extern
+#endif
 /*
  * for master
  */
-extern bool hako_master_init();
-extern bool hako_master_execute();
-extern void hako_master_set_config_simtime(hako_time_t max_delay_time_usec, hako_time_t delta_time_usec);
-extern hako_time_t hako_master_get_max_deltay_time_usec();
-extern hako_time_t hako_master_get_delta_time_usec();
+	HAKO_API bool hako_master_init();
+	HAKO_API bool hako_master_execute();
+	HAKO_API void hako_master_set_config_simtime(hako_time_t max_delay_time_usec, hako_time_t delta_time_usec);
+	HAKO_API hako_time_t hako_master_get_max_deltay_time_usec();
+	HAKO_API hako_time_t hako_master_get_delta_time_usec();
 
 /*
  * for asset
  */
-extern bool hako_asset_init();
-extern bool hako_asset_register(const char* name, hako_asset_callback_t *callbacks);
+	HAKO_API bool hako_asset_init();
+	HAKO_API bool hako_asset_register(const char* name, hako_asset_callback_t *callbacks);
 
-extern bool hako_asset_register_polling(const char* name);
-extern int  hako_asset_get_event(const char* name);
+	HAKO_API bool hako_asset_register_polling(const char* name);
+	HAKO_API int  hako_asset_get_event(const char* name);
 
-extern bool hako_asset_unregister(const char* name);
-extern void hako_asset_notify_simtime(const char* name, hako_time_t simtime);
-extern hako_time_t hako_asset_get_worldtime();
+	HAKO_API bool hako_asset_unregister(const char* name);
+	HAKO_API void hako_asset_notify_simtime(const char* name, hako_time_t simtime);
+	HAKO_API hako_time_t hako_asset_get_worldtime();
 
-extern bool hako_asset_start_feedback(const char* asset_name, bool isOk);
-extern bool hako_asset_stop_feedback(const char* asset_name, bool isOk);
-extern bool hako_asset_reset_feedback(const char* asset_name, bool isOk);
+	HAKO_API bool hako_asset_start_feedback(const char* asset_name, bool isOk);
+	HAKO_API bool hako_asset_stop_feedback(const char* asset_name, bool isOk);
+	HAKO_API bool hako_asset_reset_feedback(const char* asset_name, bool isOk);
 
 
-extern bool hako_asset_create_pdu_channel(HakoPduChannelIdType channel_id, size_t pdu_size);
-extern bool hako_asset_create_pdu_lchannel(const char* robo_name, HakoPduChannelIdType channel_id, size_t pdu_size);
-extern HakoPduChannelIdType hako_asset_get_pdu_channel(const char* robo_name, HakoPduChannelIdType channel_id);
-extern bool hako_asset_is_pdu_dirty(const char* asset_name, const char* robo_name, HakoPduChannelIdType channel_id);
-extern bool hako_asset_write_pdu(const char* asset_name, const char* robo_name, HakoPduChannelIdType channel_id, const char *pdu_data, size_t len);
-extern bool hako_asset_read_pdu(const char* asset_name, const char* robo_name, HakoPduChannelIdType channel_id, char *pdu_data, size_t len);
-extern void hako_asset_notify_read_pdu_done(const char* asset_name);
-extern void hako_asset_notify_write_pdu_done(const char* asset_name);
-extern bool hako_asset_is_pdu_sync_mode(const char* asset_name);
-extern bool hako_asset_is_simulation_mode();
-extern bool hako_asset_is_pdu_created();
+	HAKO_API bool hako_asset_create_pdu_channel(HakoPduChannelIdType channel_id, size_t pdu_size);
+	HAKO_API bool hako_asset_create_pdu_lchannel(const char* robo_name, HakoPduChannelIdType channel_id, size_t pdu_size);
+	HAKO_API HakoPduChannelIdType hako_asset_get_pdu_channel(const char* robo_name, HakoPduChannelIdType channel_id);
+	HAKO_API bool hako_asset_is_pdu_dirty(const char* asset_name, const char* robo_name, HakoPduChannelIdType channel_id);
+	HAKO_API bool hako_asset_write_pdu(const char* asset_name, const char* robo_name, HakoPduChannelIdType channel_id, const char *pdu_data, size_t len);
+	HAKO_API bool hako_asset_read_pdu(const char* asset_name, const char* robo_name, HakoPduChannelIdType channel_id, char *pdu_data, size_t len);
+	HAKO_API void hako_asset_notify_read_pdu_done(const char* asset_name);
+	HAKO_API void hako_asset_notify_write_pdu_done(const char* asset_name);
+	HAKO_API bool hako_asset_is_pdu_sync_mode(const char* asset_name);
+	HAKO_API bool hako_asset_is_simulation_mode();
+	HAKO_API bool hako_asset_is_pdu_created();
 
 
 /*
  * for simevent
  */
-extern bool hako_simevent_init();
-extern int hako_simevent_get_state();
-extern bool hako_simevent_start();
-extern bool hako_simevent_stop();
-extern bool hako_simevent_reset();
+	HAKO_API bool hako_simevent_init();
+	HAKO_API int hako_simevent_get_state();
+	HAKO_API bool hako_simevent_start();
+	HAKO_API bool hako_simevent_stop();
+	HAKO_API bool hako_simevent_reset();
 
 #ifdef __cplusplus
 }
