67688ae3cc1d8404737fc5299882f30b6bd44f8a
Takashi Mori <kanetugu2015@gmail.com>
2024-02-23 09:54:22 +0900

    add installer for win

---
+++ hako-mmap-set.bat
@@ -0,0 +1,54 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "default_shm_type=mmap"
+set "default_core_mmap_path=%CD%\mmap"
+set "config_file=%CD%\cpp_core_config.json"
+set "reset="
+set "path_to="
+
+:parse_args
+if "%1"=="" goto validate
+if "%1"=="-r" set "reset=1" & shift & goto parse_args
+if "%1"=="-p" set "path_to=%~f2" & shift & shift & goto parse_args
+goto usage
+
+:validate
+if defined reset goto check_file
+if not defined path_to goto usage
+
+:check_file
+if not exist "%config_file%" (
+    echo Error: Configuration file does not exist at %config_file%
+    exit /b 1
+)
+
+rem PowerShellがインストールされているか確認
+powershell -command "Get-Command jq" >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Error: jq is not installed.
+    echo Please install jq to use this script. You can download jq from https://stedolan.github.io/jq/download/
+    exit /b 1
+)
+
+if defined reset (
+    powershell -command "(Get-Content '%config_file%' | ConvertFrom-Json | ForEach-Object { $_.shm_type = '%default_shm_type%'; $_.core_mmap_path = '%default_core_mmap_path%'; $_ } | ConvertTo-Json) | Set-Content '%config_file%'"
+    echo Configuration reset to default.
+    goto end
+)
+
+rem カスタムパスで更新
+powershell -command "(Get-Content '%config_file%' | ConvertFrom-Json | ForEach-Object { $_.shm_type = 'mmap'; $_.core_mmap_path = '%path_to%'; $_ } | ConvertTo-Json) | Set-Content '%config_file%'"
+echo Configuration updated with custom path.
+
+:end
+echo INFO: updated %config_file%
+goto eof
+
+:usage
+echo Usage:
+echo   %0 [-r] -p /path/to  # Set custom path or reset to default
+exit /b 1
+
+:eof
+endlocal
---
+++ install.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+
+set "BUILD_REL_PATH=out\build\x64-Debug\core\sample\base-procs\hako-cmd"
+set "BIN_DIR=%CD%\bin"
+set "MY_CONFIG_PATH=%CD%\cpp_core_config.json"
+
+if not exist "%BIN_DIR%" mkdir "%BIN_DIR%"
+xcopy "%CD%\%BUILD_REL_PATH%\*.exe" "%BIN_DIR%" /D /Y
+
+setx PATH "%PATH%;%BIN_DIR%"
+setx HAKO_CONFIG_PATH "%MY_CONFIG_PATH%"
+
+echo Installation completed successfully. Please restart your terminal or log off and log back in for the changes to take effect.
+endlocal
