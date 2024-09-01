aaa5d691b993a7a1181eb2154d1d8804855de81c
Takashi Mori <kanetugu2015@gmail.com>
2024-09-01 12:35:13 +0900

    add elixir project

---
+++ bindings/elixir_project/lib/hako_asset.ex
@@ -0,0 +1,18 @@
+defmodule HakoAsset do
+  @moduledoc """
+  Documentation for `HakoAsset`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HakoAsset.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ bindings/elixir_project/test/hako_asset_test.exs
@@ -0,0 +1,8 @@
+defmodule HakoAssetTest do
+  use ExUnit.Case
+  doctest HakoAsset
+
+  test "greets the world" do
+    assert HakoAsset.hello() == :world
+  end
+end
