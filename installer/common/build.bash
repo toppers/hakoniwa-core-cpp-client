d696c60abd0323a18cdd5f3c22ac9b5208b60a9a
Takashi Mori <kanetugu2015@gmail.com>
2024-09-29 12:53:14 +0900

    bug fixed

--- installer/common/build.bash
+++ installer/common/build.bash
@@ -5,7 +5,12 @@ then
     mkdir -p cmake-build
 fi
 source setup.bash
-echo "DYLD_LIBRARY_PATH: $DYLD_LIBRARY_PATH"
+if [ `uname` = "Darwin" ]
+then
+    echo "DYLD_LIBRARY_PATH: $DYLD_LIBRARY_PATH"
+else
+    echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+fi
 cd cmake-build
 cmake ..
 make
